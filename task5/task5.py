cd402d95186b3c235a7b0cb4ebc5683fe9db0cd6
Jordan Zeitlin <78878912+DiorChoppa@users.noreply.github.com>
2022-11-22 11:39:03 +0300

    function rename

--- task5/task5.py
+++ task5/task5.py
@@ -1,7 +1,7 @@
 import json
 import numpy as np
 
-def task5(ranking_str_1: str, ranking_str_2:str) -> str:
+def task(ranking_str_1: str, ranking_str_2:str) -> str:
     ranking1 = json.loads(ranking_str_1)
     ranking2 = json.loads(ranking_str_2)
 
